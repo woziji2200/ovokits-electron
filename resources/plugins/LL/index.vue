79082ebd72befe7bfee701b0dd3f42c00d33db76
li <2633290579@qq.com>
2024-11-05 14:15:50 +0800

    LL_简单翻译文本

--- resources/plugins/LL/index.vue
+++ resources/plugins/LL/index.vue
@@ -16,18 +16,20 @@ const count = ref(0)
     <!-- 选择目标语言 -->
     <select v-model="targetLanguage">
       <option value="en">英语</option>
+      <option value="zh">汉语</option>
+      <option value="ko">朝鲜语</option>
       <option value="es">西班牙语</option>
       <option value="fr">法语</option>
       <option value="de">德语</option>
-      <option value="zh">中文</option>
-      <!-- 添加更多语言选项 -->
+      <!-- 待添加更多语言选项 -->
     </select>
 
     <!-- 翻译按钮 -->
     <button @click="translateText">翻译</button>
+    <!-- <button @click="testAwait">测试</button> -->
 
     <!-- 翻译结果 -->
-    <div v-if="translateText" class="result">
+    <div v-if="translatedText || testContent" class="result">
       <h2>翻译结果</h2>
       <p>{{ translatedText }}</p>
     </div>
@@ -35,11 +37,88 @@ const count = ref(0)
 </template>
 
 <script setup>
-import { ref } from 'vue'
+
+const { ref } = require('vue');
+const axios = require('../_libs/axios.js');
+// import axios from '../_libs/axios/axios'
+// import "../_libs/axios/dist/axios"
+
 
 const text = ref('')
-const targetLanguage = ref('en')
+const targetLanguage = ref('zh')
 const translatedText = ref('')
+const sourceLanguage = ref('')  //识别输入内容的语言
+// const testContent = ref('')
+
+
+const Deepl_AuthKey = '995e054d-bb05-4d08-a2d2-439c47880c0d:dp'
+
+// 测试请求
+const testAwait = async () => {
+  try{
+    const response = await axios.post(
+      'https://jsonplaceholder.typicode.com/posts',
+      {
+        title: 'test',
+        body: 'test内容',
+        userId: 1,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    console.log("请求成功，返回数据:", response.data);
+    testContent.value = JSON.stringify(response.data, null, 2);
+  } catch(error){
+    console.error("请求失败:", error.message);
+    testContent.value = "请求失败请检查控制台";
+  }
+}
+
+// 翻译文本
+const translateText = async () => {
+    // console.log("translateText开始");
+    // console.log("targetLanguage：", targetLanguage.value);
+    // console.log("text：", text.value);
+
+    if(!text.value){
+        translatedText.value = "请输入内容进行翻译！";
+        return;
+    }
+
+    try{
+        const response = await axios.post(
+            'https://api.deepl-pro.com/v2/translate',
+            new URLSearchParams({
+                text: text.value,
+                target_lang: targetLanguage.value
+            }),
+            
+            {
+                headers:{
+                    'Authorization': `DeepL-Auth-Key ${Deepl_AuthKey}`,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }
+        );
+        // console.log("得到: ", response.data.translations);
+        // console.log("得到: ", response.data.translations[0].text);
+        translatedText.value = response.data.translations[0].text;
+        sourceLanguage.value = response.data.translations[0].detected_source_language;
+      
+
+    }catch (error) {
+    console.error("翻译失败:", error.message);
+    translatedText.value = "翻译失败，请重试。";
+    sourceLanguage.value = '';
+  }
+}
+
+
+
 </script>
 
 <style scoped>
